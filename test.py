bdaf2d9da8b5dbaacad11fdabcf4aff1817e692f
Ryan Wallace <ryancswallace@gmail.com>
2023-01-05 06:55:13 -0500

    init commit

---
+++ test.py
@@ -0,0 +1,29 @@
+"""
+"""
+import pandas as pd
+
+rdf_metadata_url = "https://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2"
+csv_metadata_url = "https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv.gz"
+
+def download_metadata():
+    """
+    """
+    df = pd.read_csv(csv_metadata_url)"""
+"""
+import pandas as pd
+
+rdf_metadata_url = "https://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2"
+csv_metadata_url = "https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv.gz"
+
+def download_metadata():
+    """
+    """
+    df = pd.read_csv(csv_metadata_url)
+
+
+if __name__ == "__main__":
+    download_metadata()
+
+
+if __name__ == "__main__":
+    download_metadata()
